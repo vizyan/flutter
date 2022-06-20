c29dc8388f0714c50bc8b2637e2ed33bf2b7c41f
vizyan <vizyan.1@gmail.com>
2022-06-20 14:34:05 +0700

    bigger text

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter'),
-        ),
-        body: Center(
-          child: Heading(
-            text: "Hello Flutter!",
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
@@ -36,6 +27,74 @@ class Heading extends StatelessWidget {
         fontSize: 24,
         fontWeight: FontWeight.bold,
       ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Screen')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text(
+            'Next',
+            style: TextStyle(fontSize: 24.0),
+          ),
+          onPressed: () {
+            _navigateToNextScreen(context);
+          },
+        ),
+      ),
+    );
+  }
+
+  void _navigateToNextScreen(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => NewScreen()));
+  }
+}
+
+class NewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('New Screen')),
+      body: Center(
+        child: BiggerText(text: "Hello Flutter!"),
+      ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({required this.text});
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
